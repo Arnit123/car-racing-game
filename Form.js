eabc973fa0c84ad24169cf01211cee4efcd2ae64
Arnit123 <72302772+Arnit123@users.noreply.github.com>
2020-11-19 14:22:12 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,26 @@
+class Form{
+    constructor(){
+
+    }
+  display(){
+   var title = createElement('h2') 
+   title.html("Car Racing Game")
+   title.position(230,20);
+   var input = createInput("name")
+   input.position(130,160);
+   var button = createButton('play')
+   button.position(250,200);
+   button.mousePressed(function(){
+    title.hide();
+    input.hide();
+    button.hide();   
+    var name = input.value();
+    playerCount+= 1
+    player.update(name);
+    player.updateCount(playerCount);
+    var greeting = createElement('h3')
+    greeting.html("Hello "+name+" !")
+    greeting.position(260,20)
+   }) 
+}  
+}
